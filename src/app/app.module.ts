9d0daf3bd9d38f71d038ba4c30e6cdf625715e19
kgotgit <kgotgit>
2020-03-04 12:22:56 -0600

    added custom element hello world js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { NgModule, Injector } from '@angular/core';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import {createCustomElement} from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule
   ],
+  entryComponents:[HelloWorldComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  /* bootstrap: [AppComponent] */
 })
-export class AppModule { }
+export class AppModule {
+  constructor(injector: Injector) {
+    const custom = createCustomElement(HelloWorldComponent, {injector: injector});
+    customElements.define('app-hello-world', custom);
+  }
+
+  ngDoBootstrap() {
+
+  }
+}
